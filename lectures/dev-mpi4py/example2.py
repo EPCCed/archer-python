ebdb893e132b9617955e185830e56ead100912b2
Adrian Jackson <a.jackson@epcc.ed.ac.uk>
2019-12-17 14:21:52 +0000

    Updating order of the mpi4py lecture so communicator stuff comes at the end not the beginning

--- lectures/dev-mpi4py/example2.py
+++ lectures/dev-mpi4py/example2.py
@@ -1,23 +1,22 @@
-"""Creating a Cartesian Communicator"""
+"""A simple blocking Send/Recv pair"""
 
 import sys
+import numpy
 from mpi4py import MPI
 
-def main(parent):
-    """Create a 2-d Cartesian communicator from parent communicator"""
-
-    dims = MPI.Compute_dims(parent.size, 2)
-
-    comm = parent.Create_cart(dims, periods = [True, False])
+def main(comm, sz):
+    """Send a message between ranks 0 and 1"""
     rank = comm.Get_rank()
-    coords = comm.Get_coords(rank)
-    upx = comm.Shift(0, 1)
-    upy = comm.Shift(1, 1)
 
-    out = "Rank{:2d} coords{:2d} {:2d} upx(src,dst) {} upy(src,dst) {}\n"\
-                         .format(rank, coords[0], coords[1], upx, upy)
-    sys.stdout.write(out)
+    if rank == 0:
+        msg = numpy.ones(sz, numpy.double)
+        comm.Ssend([msg, sz, MPI.DOUBLE], dest = 1, tag = 999)
+    elif rank == 1:
+        msg = numpy.zeros(sz, numpy.double)
+        comm.Recv([msg, sz, MPI.DOUBLE], source = 0, tag = 999)
+        sys.stdout.write("Rank 1 received {}".format(msg))
 
 if __name__ == "__main__":
-    """Execute in MPI.COMM_WORLD"""
-    main(MPI.COMM_WORLD)
+
+    sz = 4
+    main(MPI.COMM_WORLD, sz)
--- lectures/dev-mpi4py/example4.py
+++ lectures/dev-mpi4py/example4.py
@@ -1,29 +1,17 @@
-"""An Example of non-blocking Isend/Irecv pairs"""
+"""Message passing a python object"""
 
 import sys
-import numpy
 from mpi4py import MPI
 
 def main(comm):
-    """Exchange messages with 'ajoining' ranks"""
-    p1 = comm.rank + 1
-    m1 = comm.rank - 1
-    if p1 >= comm.size: p1 = 0
-    if m1 < 0: m1 = comm.size - 1
+    """Send a list from rank 0 to rank 1"""
 
-    smsg = numpy.array([comm.rank], numpy.int)
-    rmsg = numpy.zeros(2, numpy.int)
-
-    reqs1 = comm.Issend(smsg, p1)
-    reqs2 = comm.Issend(smsg, m1)
-    reqr1 = comm.Irecv(rmsg[0:], source = p1)
-    reqr2 = comm.Irecv(rmsg[1:], source = m1)
-
-    reqr1.Wait(); reqr2.Wait()
-    sys.stdout.write("Rank {} received messages from ranks {} and {}\n"\
-                         .format(smsg, rmsg[1], rmsg[0]))
-
-    MPI.Request.Waitall([reqs1, reqs2])
+    if comm.rank == 0:
+        msg = ["Any", "old", "thing", comm.rank, {"size" : comm.size}]
+        comm.send(msg, dest = 1, tag = 999)
+    elif comm.rank == 1:
+        msg = comm.recv(source = 0, tag = 999)
+        sys.stdout.write("Rank 1 received {}\n".format(msg))
 
 if __name__ == "__main__":
 
